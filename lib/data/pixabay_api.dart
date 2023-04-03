cae5e83d7a94c422d540d7d7a5679ff87c14ae9c
ParkJuneWoo <krca0220@gmail.com>
2023-04-03 19:34:43 +0900

    API Mockito 테스트 추가

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -9,8 +9,9 @@ class PixabayApi implements PhotoApiRepository {
   final key = "35017077-95d0aae89ecfa8fc1123ed7f7";
 
   @override
-  Future<List<Photo>> fetch(String query) async {
-    var res = await http
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    client ??= http.Client();
+    var res = await client
         .get(Uri.parse("$baseUrl?key=$key&q=$query&image_type=photo"));
 
     final result = jsonDecode(res.body);
