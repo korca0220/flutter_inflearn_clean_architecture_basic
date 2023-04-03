edb614f9c909c1a466e8f1835d119be0d9cb9573
ParkJuneWoo <krca0220@gmail.com>
2023-04-03 19:55:16 +0900

    Json_Serializable, Equtable 추가

--- lib/models/photo_model.dart
+++ lib/models/photo_model.dart
@@ -1,114 +1,24 @@
-import 'dart:convert';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-Photo photoFromJson(String str) => Photo.fromJson(json.decode(str));
+part 'photo_model.g.dart';
 
-String photoToJson(Photo data) => json.encode(data.toJson());
-
-class Photo {
-  Photo({
+@JsonSerializable()
+class Photo extends Equatable {
+  final int id;
+  final String tags;
+  @JsonKey(name: 'previewURL')
+  final String previewUrl;
+  const Photo({
     required this.id,
-    required this.pageUrl,
-    required this.type,
     required this.tags,
     required this.previewUrl,
-    required this.previewWidth,
-    required this.previewHeight,
-    required this.webformatUrl,
-    required this.webformatWidth,
-    required this.webformatHeight,
-    required this.largeImageUrl,
-    required this.fullHdurl,
-    required this.imageUrl,
-    required this.imageWidth,
-    required this.imageHeight,
-    required this.imageSize,
-    required this.views,
-    required this.downloads,
-    required this.likes,
-    required this.comments,
-    required this.userId,
-    required this.user,
-    required this.userImageUrl,
   });
 
-  int id;
-  String pageUrl;
-  String type;
-  String tags;
-  String previewUrl;
-  int previewWidth;
-  int previewHeight;
-  String webformatUrl;
-  int webformatWidth;
-  int webformatHeight;
-  String largeImageUrl;
-  String fullHdurl;
-  String imageUrl;
-  int imageWidth;
-  int imageHeight;
-  int imageSize;
-  int views;
-  int downloads;
-  int likes;
-  int comments;
-  int userId;
-  String user;
-  String userImageUrl;
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
 
-  factory Photo.fromJson(Map<String, dynamic> json) => Photo(
-        id: json["id"],
-        pageUrl: json["pageURL"],
-        type: json["type"],
-        tags: json["tags"],
-        previewUrl: json["previewURL"],
-        previewWidth: json["previewWidth"],
-        previewHeight: json["previewHeight"],
-        webformatUrl: json["webformatURL"],
-        webformatWidth: json["webformatWidth"],
-        webformatHeight: json["webformatHeight"],
-        largeImageUrl: json["largeImageURL"],
-        fullHdurl: json["fullHDURL"] ?? '',
-        imageUrl: json["imageURL"] ?? '',
-        imageWidth: json["imageWidth"],
-        imageHeight: json["imageHeight"],
-        imageSize: json["imageSize"],
-        views: json["views"],
-        downloads: json["downloads"],
-        likes: json["likes"],
-        comments: json["comments"],
-        userId: json["user_id"],
-        user: json["user"],
-        userImageUrl: json["userImageURL"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "pageURL": pageUrl,
-        "type": type,
-        "tags": tags,
-        "previewURL": previewUrl,
-        "previewWidth": previewWidth,
-        "previewHeight": previewHeight,
-        "webformatURL": webformatUrl,
-        "webformatWidth": webformatWidth,
-        "webformatHeight": webformatHeight,
-        "largeImageURL": largeImageUrl,
-        "fullHDURL": fullHdurl,
-        "imageURL": imageUrl,
-        "imageWidth": imageWidth,
-        "imageHeight": imageHeight,
-        "imageSize": imageSize,
-        "views": views,
-        "downloads": downloads,
-        "likes": likes,
-        "comments": comments,
-        "user_id": userId,
-        "user": user,
-        "userImageURL": userImageUrl,
-      };
+  Map<String, dynamic> toJson() => _$PhotoToJson(this);
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Photo && runtimeType == other.runtimeType && id == other.id;
+  List<Object?> get props => [id];
 }
