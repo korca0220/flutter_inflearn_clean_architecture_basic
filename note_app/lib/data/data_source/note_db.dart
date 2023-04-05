0ec04cc1ab4de02da0b65610ab165331f2408dd9
Junewoo <krca02220@gmail.com>
2023-04-06 01:51:02 +0900

    db update

---
+++ note_app/lib/data/data_source/note_db.dart
@@ -0,0 +1,44 @@
+import 'package:note_app/domain/model/note.dart';
+import 'package:sqflite/sqflite.dart';
+
+class NoteDB {
+  Database db;
+  NoteDB(this.db);
+
+  Future<Note?> getNoteById(int id) async {
+    // SELECT = FROM note WHERE id = ?
+    final result = await db.query(
+      'note',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (result.isNotEmpty) {
+      return Note.fromJson(result.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<Note>> getNotes() async {
+    final result = await db.query('note');
+    return result.map((e) => Note.fromJson(e)).toList();
+  }
+
+  Future<void> insertNote(Note note) async {
+    await db.insert('note', note.toJson());
+  }
+
+  Future<void> updateNote(Note note) async {
+    await db.update(
+      'note',
+      note.toJson(),
+      where: 'id =?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<void> deleteNote(Note note) async {
+    await db.delete('note', where: 'id = ?', whereArgs: [note.id]);
+  }
+}
--- note_app/lib/data/repository/note_repository_impl.dart
+++ note_app/lib/data/repository/note_repository_impl.dart
@@ -1,37 +1,33 @@
-
-
+import 'package:note_app/data/data_source/note_db.dart';
 import 'package:note_app/domain/model/note.dart';
 import 'package:note_app/domain/repository/note_repository.dart';
 
-class NoteRepositoryImpl implements NoteRepository{
+class NoteRepositoryImpl implements NoteRepository {
+  final NoteDB db;
+  NoteRepositoryImpl(this.db);
+
   @override
-  Future<void> deleteNote(Note note) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
   }
 
   @override
-  Future<void> editNote(Note note) {
-    // TODO: implement editNote
-    throw UnimplementedError();
+  Future<void> editNote(Note note) async {
+    await db.updateNote(note);
   }
 
   @override
-  Future<Note> getNoteById(int id) {
-    // TODO: implement getNoteById
-    throw UnimplementedError();
+  Future<Note?> getNoteById(int id) async {
+    return await db.getNoteById(id);
   }
 
   @override
-  Future<List<Note>> getNotes() {
-    // TODO: implement getNotes
-    throw UnimplementedError();
+  Future<List<Note>> getNotes() async {
+    return db.getNotes();
   }
 
   @override
-  Future<void> insertNote(Note note) {
-    // TODO: implement insertNote
-    throw UnimplementedError();
+  Future<void> insertNote(Note note) async {
+    return db.insertNote(note);
   }
-
 }
